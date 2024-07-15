dbeeb9653256909b4cc158052527cc970e4e2522
Grenish rai <ariestheracer@gmail.com>
2024-07-16 01:53:58 +0530

    feat: Add GetStarted page to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import TestPage from "./pages/TestPage";
 import HomePage from "./pages/Home";
+import GetStarted from "./pages/GetStarted";
 
 export default function Home() {
   return (
     <>
       <HomePage />
+      <GetStarted />
     </>
   );
 }
