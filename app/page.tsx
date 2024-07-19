1f89bf865075c6e937436c5b5acfd2d634c39e17
Grenish rai <ariestheracer@gmail.com>
2024-07-20 01:55:04 +0530

    feat: Add Navbar component to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import TestPage from "./pages/TestPage";
 import HomePage from "./pages/Home";
 import GetStarted from "./pages/GetStarted";
+import Navbar from "./pages/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <HomePage />
       <GetStarted />
     </>
